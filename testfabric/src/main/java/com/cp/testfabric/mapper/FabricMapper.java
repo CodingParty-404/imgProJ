f2d2332f20f330d923372fc6cdd39148cabfb685
hyejin <hjleesig@gmail.com>
2020-06-10 17:02:59 +0900

    aAA

--- testfabric/src/main/java/com/cp/testfabric/mapper/FabricMapper.java
+++ testfabric/src/main/java/com/cp/testfabric/mapper/FabricMapper.java
@@ -1,11 +1,11 @@
 package com.cp.testfabric.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+
 
 @Mapper
 public interface FabricMapper {
     
-    @Select("select now()")
+    // @Select("select now()")
     public String timeSelect();
 }
